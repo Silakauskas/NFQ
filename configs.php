4b5fb7059ee76ec4bae190f8309738679e53e483
DESKTOP-GOIP0HF\Simonas <simonas.silakauskas@gmail.com>
2017-09-17 12:32:00 +0300

    no message

--- configs.php
+++ configs.php
@@ -3,9 +3,9 @@
  define('DB_USER', 'root');
  define('DB_PASS', 'root');
  define('DB_HOST', 'localhost');*/
- $DB = "dbname = dcfsu68gb20t2s";
- $DB_USER = "user = ycbiiheljhbeii";
- $DB_PASS = "password = 0186292106d793023f5d6c42f7288cac7f36d962ff6824b08714eb94e8f3e98b";
- $DB_HOST = "host = ec2-46-137-117-43.eu-west-1.compute.amazonaws.com";
- $PORT = "port = 5432";
+ $DB = "dcfsu68gb20t2s";
+ $DB_USER = "ycbiiheljhbeii";
+ $DB_PASS = "0186292106d793023f5d6c42f7288cac7f36d962ff6824b08714eb94e8f3e98b";
+ $DB_HOST = "ec2-46-137-117-43.eu-west-1.compute.amazonaws.com";
+ $PORT = "5432";
  ?>
